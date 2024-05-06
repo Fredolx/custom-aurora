027b069d7cceeede096771433e54765f58a4ce17
Frédéric Lachapelle <38048855+Fredolx@users.noreply.github.com>
2024-05-06 16:23:01 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,9 +12,13 @@ RELEASE="$(rpm -E %fedora)"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-rpm-ostree install mpv yt-dlp
-rpm-ostree uninstall sddm-kcm sddm kde-settings-sddm sddm-breeze
+rpm-ostree install mpv yt-dlp helix
+
+rpm-ostree uninstall sddm-kcm sddm kde-settings-sddm sddm-breeze akonadi-server akonadi-server-mysql amd-gpu-firmware \
+cockpit-bridge cockpit-machines cockpit-networkmanager cockpit-ostree cockpit-pcp cockpit-podman cockpit-selinux cockpit-storaged cockpit-system \
+plasma-browser-integration fedora-chromium-config-kde mariadb-server mariadb-server-utils mariadb-gssapi-server mariadb-cracklib-password-check mariadb-backup \
+mariadb-common mariadb-errmsg mariadb mariadb-connector-c qt6-qtbase-mysql perl-DBD-MariaDB mariadb-connector-c-config \
+rocm-comgr rocm-runtime rocminfo rocm-device-libs rocm-hip rocm-opencl hipcc plymouth plymouth-core-libs plymouth-graphics-libs plymouth-plugin-label \
+plymouth-plugin-two-step plymouth-scripts plymouth-system-theme plymouth-theme-spinner wireguard-tools 
+ 
 sudo systemctl set-default multi-user.target
-# this would install a package from rpmfusion
-# rpm-ostree install vlc
