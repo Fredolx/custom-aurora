9732eb68cfdf337b03a03fc1b7154977ef978044
Frédéric Lachapelle <38048855+Fredolx@users.noreply.github.com>
2024-05-06 16:52:38 -0400

    remove firewall

--- build.sh
+++ build.sh
@@ -11,7 +11,6 @@ RELEASE="$(rpm -E %fedora)"
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
 rpm-ostree install mpv yt-dlp helix
 
 rpm-ostree uninstall sddm-kcm sddm kde-settings-sddm sddm-breeze akonadi-server akonadi-server-mysql amd-gpu-firmware \
@@ -19,6 +18,7 @@ cockpit-bridge cockpit-machines cockpit-networkmanager cockpit-ostree cockpit-pc
 plasma-browser-integration fedora-chromium-config-kde mariadb-server mariadb-server-utils mariadb-gssapi-server mariadb-cracklib-password-check mariadb-backup \
 mariadb-common mariadb-errmsg mariadb mariadb-connector-c qt6-qtbase-mysql perl-DBD-MariaDB mariadb-connector-c-config \
 rocm-comgr rocm-runtime rocminfo rocm-device-libs rocm-hip rocm-opencl hipcc plymouth plymouth-core-libs plymouth-graphics-libs plymouth-plugin-label \
-plymouth-plugin-two-step plymouth-scripts plymouth-system-theme plymouth-theme-spinner wireguard-tools 
+plymouth-plugin-two-step plymouth-scripts plymouth-system-theme plymouth-theme-spinner wireguard-tools firewalld firewalld-filesystem firewall-config \
+python3-firewall python3-nftables 
  
 sudo systemctl set-default multi-user.target
